b2f5d34def2fe9f4025f6cd5bf451624c90dd12d
ConstantRen <Rmarianoconstantino@gmail.com>
2025-02-09 19:22:39 +0800

    latest

--- http-client-prg/src/app/app.component.ts
+++ http-client-prg/src/app/app.component.ts
@@ -1,13 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { CommonModule } from '@angular/common';
-import {}  
+import { HttpclientService } from './httpclient.service';  
+
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
+
 export class AppComponent {
   title = 'http-client-prg';
+  httpusers: any;
+
+  constructor(private httpclientList: HttpclientService){
+
+    this.httpclientList.getUsersRemotely().subscribe((data:any)=> {
+      this.httpusers = data.slice(0,5);
+    })
+
+  }
 }
